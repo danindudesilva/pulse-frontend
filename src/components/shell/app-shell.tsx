1c588789f135c5ac899203fe31f84d9cb4070c93
Danindu de Silva <danindu001@gmail.com>
2026-03-26 14:50:09 +0000

    fix: revert temporary mobile top navigation

--- src/components/shell/app-shell.tsx
+++ src/components/shell/app-shell.tsx
@@ -1,4 +1,3 @@
-import { AppMobileNav } from "@/components/shell/app-mobile-nav";
 import { AppSidebar } from "@/components/shell/app-sidebar";
 
 type AppShellProps = {
@@ -10,10 +9,7 @@ export function AppShell({ children }: AppShellProps) {
     <div className="min-h-screen bg-[var(--background)]">
       <div className="flex min-h-screen">
         <AppSidebar />
-        <div className="flex min-w-0 flex-1 flex-col">
-          <AppMobileNav />
-          {children}
-        </div>
+        <div className="flex min-w-0 flex-1 flex-col">{children}</div>
       </div>
     </div>
   );
